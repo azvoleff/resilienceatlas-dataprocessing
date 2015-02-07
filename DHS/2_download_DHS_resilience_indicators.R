c362b8beead81f9a597fddfbb964bfe2b2e56982
Alex Zvoleff <azvoleff@gmail.com>
2015-02-07 17:46:19 -0500

    Finalize resilience indicator.

--- DHS/2_download_DHS_resilience_indicators.R
+++ DHS/2_download_DHS_resilience_indicators.R
@@ -1,5 +1,5 @@
-require(RJSONIO)
-require(dplyr)
+library(RJSONIO)
+library(dplyr)
 
 source('../0_settings.R')
 
@@ -12,6 +12,12 @@ indicator_list <- lapply(indicator_list$Data, function(x) {unlist(x)})
 # Convert JSON input to a data frame
 indicator_list <- as.data.frame(do.call("rbind", indicator_list), stringsAsFactors=FALSE)
 
+country_list <- fromJSON('http://api.dhsprogram.com/rest/dhs/countries?APIkey=CVINTL-877527?returnFields=countryId,Label,Definition')
+# Unlist the JSON file entries
+country_list <- lapply(country_list$Data, function(x) {unlist(x)})
+# Convert JSON input to a data frame
+country_list <- as.data.frame(do.call("rbind", country_list), stringsAsFactors=FALSE)
+
 grep_list <- function(x) {
     def_rows <- grepl(tolower(x), tolower(indicator_list$Label))
     label_rows <- grepl(tolower(x), tolower(indicator_list$Definition))
@@ -22,15 +28,23 @@ grep_list('water')
 
 ###############################################################################
 ###  Build indicator
- 
-get_indic <- function(indicator_IDs) {
+
+grp_list <- read.csv(file.path(prefix, "GRP", "GRP_Countries.csv"))
+# Filter for only GRP countries
+grp_list <- grp_list[grp_list$GRP == "Yes", ]
+# Filter for only countries with DHS data available
+grp_list <- grp_list[grp_list$ISO_Code %in% country_list$ISO2_CountryCode, ]
+
+grp_list <- merge(grp_list, country_list, by.x="ISO_Code", by.y="ISO2_CountryCode")
+
+ccs <- as.character(grp_list$DHS_CountryCode)
+
+get_indic <- function(indicator_IDs, ccs) {
     # Note DHS uses "NI" for Niger instead of the correct, "NE")
-    ccs <- c("ER", "ET", "ID", "NI", "UG")
     cc_string <- paste0("countryIds=", paste(ccs, collapse=","))
     indicators_string <- paste0("indicatorIds=", paste(indicator_IDs, collapse=","))
     api_base <- "http://api.dhsprogram.com/rest/dhs/data?breakdown=subnational&APIkey=CVINTL-877527&perpage=5000"
     api_call <- paste0(api_base, "&", indicators_string, "&", cc_string, "&f=json")
-    # Import DHS Indicator data for TFR for each survey
     json_file <- fromJSON(api_call)
     # Unlist the JSON file entries
     d <- lapply(json_file$Data, function(x) {unlist(x)})
@@ -41,36 +55,59 @@ get_indic <- function(indicator_IDs) {
 }
 
 buff_indic <- c(214527002, # Children stunted
-                72254002, # Infant mortality rate
+                70254002, # Infant mortality rate
                 6136002, # Population age 6 and over who attended secondary education
-                120147001, # Household size
-                10485003, # Percent in middle wealth quintile or above (women)
-                10485004, # Percent in fourth quintile or above (women)
-                10485005) # Percent in highest quintile or above (women)
-buff <- get_indic(buff_indic)
-buff$facet <- "Buffer capacity"
+                120147001) # Household size)
+#TODO: Make assets index
+buff <- get_indic(buff_indic, ccs)
+stopifnot(nrow(buff) < 5000)
+buff$Facet <- "Buffer capacity"
+unique(buff$Indicator)
 
 sorg_indic <- c(8139010, # Piped water
                 8142001) # Access to electricity
-sorg <- get_indic(sorg_indic)
-sorg$facet <- "Self-organization"
+sorg <- get_indic(sorg_indic, ccs)
+stopifnot(nrow(sorg) < 5000)
+sorg$Facet <- "Self-organization"
+unique(sorg$Indicator)
 
 learn_indic <- c(127383002, # women who are literate
-                 7135002) #  pop 6-15 who are attending school
-learn <- get_indic(learn_indic)
-learn$facet <- "Capacity for learning"
-
+                 7135002, # pop 6-15 who are attending school
+                 9124003, # households possessing a phone
+                 13126005) # percentage women with access to newspaper, television, and radio at least once per week
+learn <- get_indic(learn_indic, ccs)
+stopifnot(nrow(learn) < 5000)
+learn$Facet <- "Capacity for learning"
+unique(learn$Indicator)
 
 indic <- full_join(buff, sorg)
 indic <- full_join(indic, learn)
 
+table(indic$CountryName == "Niger")
+table(indic$CountryName == "Niger", indic$SurveyYear)
+
+indic$SurveyYear <- as.numeric(indic$SurveyYear)
+
+indic <- indic[indic$SurveyYear > 2004, ]
+table(indic$SurveyYear)
+table(indic$CountryName)
+
+dim(indic)
 # Drop all but most recent survey data, and take mean when both men/women are 
 # supplied
 indic <- group_by(indic, CountryName) %>%
     filter(SurveyYear == max(SurveyYear)) %>%
-    group_by(facet, DHS_CountryCode, CountryName, SurveyYear, SurveyId, 
-             IndicatorId, RegionId, CharacteristicLabel) %>%
+    group_by(Facet, DHS_CountryCode, CountryName, SurveyYear, SurveyId, 
+             Indicator, IndicatorId, RegionId, CharacteristicLabel) %>%
     summarize(Value=mean(as.numeric(Value)))
+dim(indic)
+table(indic$SurveyYear)
+table(indic$CountryName)
+
+hist(indic$SurveyYear)
+
+# Add in my region names
+indic <- left_join(indic, dplyr::select(grp_list, DHS_CountryCode, Region))
 
 # Make the income variables a sum
     
@@ -78,19 +115,7 @@ indic <- group_by(indic, CountryName) %>%
 # 10485004, # Percent in fourth quintile or above (women)
 # 10485005) # Percent in highest quintile or above (women)
 
-# TODO: Add in gridded data
-# load(file.path(prefix, "GRP", "Resilience_Indicator", 
-#                "dhs_regions_merged_with_Justin_data.RData"))
-#
-# gis_data <- dhs_regions@data
-# gis_data <- gis_data[c("REG_ID", "maize_per_person", "calories_per_person")]
-# gis_data <- tbl_df(gis_data)
-#
-# dim(indic)
-# indic <- left_join(indic, gis_data, by=list("RegionId"="REG_ID"))
-# dim(indic)
-
-# Ensure all indicators are in regional format
+names(indic)[names(indic) == "Region"] <- "RegionGRP"
 
 save(indic, file=file.path(prefix, "GRP", 
                            "Resilience_Indicator","resil_raw_DHS_data.RData"))
--- DHS/3_DHS_calculate_resilience.R
+++ DHS/3_DHS_calculate_resilience.R
@@ -1,6 +1,105 @@
 library(rgdal)
 library(raster)
 library(dplyr)
+library(ggplot2)
+library(reshape2)
+library(RColorBrewer)
 
 source('../0_settings.R')
 
+# TODO: Add in gridded data
+# load(file.path(prefix, "GRP", "Resilience_Indicator", 
+#                "dhs_regions_merged_with_Justin_data.RData"))
+#
+# gis_data <- dhs_regions@data
+# gis_data <- gis_data[c("REG_ID", "maize_per_person", "calories_per_person")]
+# gis_data <- tbl_df(gis_data)
+#
+# dim(indic)
+# indic <- left_join(indic, gis_data, by=list("RegionId"="REG_ID"))
+# dim(indic)
+
+# TODO: Ensure all indicators are in regional format
+
+load(file.path(prefix, "GRP", 
+               "Resilience_Indicator","resil_raw_DHS_data.RData"))
+
+# Calculate quantiles
+probs <- seq(0, 1, .2)
+# Note the subtraction of 1 in calaculation of value quantile is so that the 
+# final scores will start from 0 after normalization
+indic <- group_by(indic, IndicatorId) %>%
+    mutate(ValueQuantile=as.numeric(cut(Value,
+                                        quantile(Value, probs, na.rm=TRUE), 
+                                        include.lowest=TRUE)) - 1) %>%
+    group_by(RegionId, Facet) %>%
+    mutate(NumValidValue=n() - sum(is.na(Value)))
+
+# Reverse quantiles where high numbers are BAD
+rev_index <- function(x) {
+    max(x) + 1 - x
+}
+# Children stunted
+# Infant mortality rate
+
+filter(indic, CountryName == "Indonesia")
+
+resil <- group_by(indic, CountryName, RegionGRP, RegionId, Facet) %>%
+    summarise(Score=sum(ValueQuantile) / (NumValidValue[1]*(length(probs) - 2)) * 10)
+
+group_by(resil, CountryName, Facet) %>%
+    summarize(mean_score=mean(Score)) %>%
+    ggplot() +
+    geom_bar(aes(CountryName, mean_score, fill=Facet), position="dodge", stat="identity") +
+    xlab("Country") + ylab("Score") + ylim(0, 10) +
+    scale_fill_discrete("Facet of resilience")
+
+resil$Facet <- factor(resil$Facet,
+                      levels=c('Buffer capacity', 'Self-organization', 
+                               'Capacity for learning'),
+                      labels=c('BuffCap', 'SelfOrg', 'CapLearn'))
+
+resil_wide <- dcast(resil, CountryName + RegionGRP + RegionId ~ Facet)
+
+load(file.path(prefix, "GRP", "Resilience_Indicator", 
+               "dhs_regions_merged_with_Justin_data.RData"))
+
+dhs_regions$RegionId_short <- gsub('^[A-Z]{5}[0-9]{4}', '', dhs_regions$REG_ID)
+resil_wide$RegionId_short <- gsub('^[A-Z]{5}[0-9]{4}', '', resil_wide$RegionId)
+
+# table(resil_wide$RegionId %in% dhs_regions$REG_ID)
+# resil[!(resil$RegionId %in% dhs_regions$REG_ID), ]
+#
+# table(resil_wide$RegionId_short %in% dhs_regions$RegionId_short)
+# resil_wide[!(resil_wide$RegionId_short %in% dhs_regions$RegionId_short), ]
+
+# Filter GIS dataset to only include regions that are in the resilience dataset
+dhs_regions <- dhs_regions[dhs_regions$RegionId_short %in% resil_wide$RegionId_short, ]
+
+dhs_regions@data <- select(dhs_regions@data, RegionId_short, maize_per_person, 
+                           calories_per_person)
+dhs_regions@data <- merge(dhs_regions@data, resil_wide)
+
+# display.brewer.all(n=10, exact.n=FALSE)
+
+dhs_regions$maize_per_person <- as.numeric(dhs_regions$maize_per_person)
+dhs_regions$calories_per_person <- as.numeric(dhs_regions$calories_per_person)
+writeOGR(dhs_regions, file.path(prefix, "GRP", "Resilience_Indicator"), 
+         "resil_indicator", driver="ESRI Shapefile")
+
+png('GRP_resilience_map.png', width=1500, heigh=1200)
+spplot(dhs_regions, c('BuffCap', 'CapLearn', 'SelfOrg'),
+       col.regions=brewer.pal(11, "PuOr"), at=seq(0, 10),
+       par.settings=list(fontsize=list(text=20)))
+dev.off()
+
+#
+# for (RegionGRP in unique(dhs_regions$RegionGRP)) {
+#     png(paste0(gsub(' ', '', RegionGRP), '_resilience_map.png'), width=1200, 
+#         heigh=900)
+#     print(spplot(dhs_regions[which(dhs_regions$RegionGRP == RegionGRP), ],
+#            c('BuffCap', 'CapLearn', 'SelfOrg'),
+#            col.regions=brewer.pal(11, "PuOr"),
+#            at=seq(0, 10)))
+#     dev.off()
+# }
