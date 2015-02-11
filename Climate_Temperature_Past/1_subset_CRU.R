08d599f2d7744a37eb177bd95709acc23287eec0
Alex Zvoleff <azvoleff@gmail.com>
2015-02-11 16:18:15 -0500

    Rename for clarity.

--- Climate_Temperature_Past/1_subset_CRU.R
+++ Climate_Temperature_Past/1_subset_CRU.R
@@ -14,25 +14,24 @@ source('../0_settings.R')
 
 overwrite <- TRUE
 
-iso_key <- read.csv(file.path("..", "ISO_Codes.csv"), na.strings="")
-
 product <- 'cru_ts3.22'
 datestring <- '1901.2013'
 
 in_folder <- file.path(prefix, "CRU", "cru_ts_3.22")
 out_folder <- file.path(prefix, "GRP", "CRU")
-shp_folder <- file.path(prefix, "GRP", "Boundaries", "National")
+shp_folder <- file.path(prefix, "GRP", "Boundaries")
 stopifnot(file_test('-d', in_folder))
 stopifnot(file_test('-d', out_folder))
 stopifnot(file_test("-d", shp_folder))
 
-#datasets <- c('tmn', 'tmx', 'tmp', 'pet')
+#datasets <- c('tmn', 'tmx', 'tmp')
 datasets <- c('tmp')
 
 # This is the projection of the CRU files
 s_srs <- '+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs +towgs84=0,0,0'
 
-ISO_2s <- c("ID", "ET", "UG", "NE", "ER")
+aoi_polygons <- readOGR(shp_folder, 'Analysis_Areas')
+aoi_polygons <- aoi_polygons[aoi_polygons$Type == "Country", ]
 
 foreach (dataset=datasets, .inorder=FALSE,
          .packages=c("teamlucc", "rgeos", "raster", "rgdal")) %dopar% {
@@ -45,17 +44,19 @@ foreach (dataset=datasets, .inorder=FALSE,
     this_dataset <- stack(ncdf)
     proj4string(this_dataset) <- s_srs
 
-    for (ISO_2 in ISO_2s) {
-        ISO_3 <- as.character(iso_key$ISO_3[match(ISO_2, iso_key$ISO_2)])
-        aoi <- readOGR(shp_folder, paste0(ISO_3, '_adm0'))
-        stopifnot(length(aoi) == 1)
+    stopifnot(file_test('-f', ncdf))
+
+    for (n in 1:nrow(aoi_polygons)) {
+        aoi <- aoi_polygons[n, ]
+        name <- as.character(aoi$Name)
+        name <- gsub(' ', '', name)
         aoi <- gConvexHull(aoi)
         aoi <- spTransform(aoi, CRS(utm_zone(aoi, proj4string=TRUE)))
         aoi <- gBuffer(aoi, width=100000)
         aoi <- spTransform(aoi, CRS(s_srs))
 
         dstfile <- file.path(out_folder,
-                              paste0(ISO_2, "_", product, '_', dataset, '_', 
+                              paste0(name, "_", product, '_', dataset, '_', 
                                      datestring,  '.tif'))
         cropped_data <- crop(this_dataset, aoi, overwrite=TRUE, filename=dstfile)
     }
