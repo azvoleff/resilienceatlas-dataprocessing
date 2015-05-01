262a74660854c4cba49edae4f4936f2f15dfc57d
Alex Zvoleff <azvoleff@gmail.com>
2015-05-01 15:24:46 -0400

    Add growth calculation.

--- Population/1_calculate_WorldPop_Pyramids.R
+++ Population/1_calculate_WorldPop_Pyramids.R
@@ -8,20 +8,30 @@ library(ggplot2)
 library(stringr)
 library(plyr)
 library(foreach)
+library(gridExtra)
 library(dplyr)
+library(RColorBrewer)
 
 dpi <- 500
 width <- 6
 height <- 3
 
-d <- read.csv("M:/Vital_Signs/GRP/data/regional/basedata/Horn of Africa/Pop Stats by Age/HornPopMergeFinal.csv")
-d$Adjusted <- grepl('_adj$', d$Age_Range_Gender)
-d$Age_Range_Gender <- gsub('(^ap00v4_)|(_adj$)', '', d$Age_Range_Gender)
-d$Category <- gsub('_', '', str_extract(d$Age_Range_Gender, "^[a-zA-Z0-9]*"))
-d$Gender <- gsub('_', '', str_extract(d$Age_Range_Gender, "(_F$)|_M$()"))
+d <- foreach(year=c(2000, 2005, 2010, 2015), .combine=rbind) %do% {
+    read.csv(paste0("M:/Vital_Signs/GRP/data/regional/basedata/Horn of Africa/Population Data/HornOfAfrica_", year, "Population.csv"))
+}
+d$COUNTRY <- ordered(d$COUNTRY, levels=sort(unique(d$COUNTRY)))
 
+d$year <- as.numeric(paste0('20', gsub('(^ap)|(v4_$)', '', str_extract(d$VARIABLE, '^ap[01][05]v4_'))))
+d$Adjusted <- grepl('_adj$', d$VARIABLE)
+d$VARIABLE <- gsub('(^ap[01][05]v4_)|(_adj$)', '', d$VARIABLE)
+d$Category <- gsub('_', '', str_extract(d$VARIABLE, "^[a-zA-Z0-9]*"))
+d$Gender <- gsub('_', '', str_extract(d$VARIABLE, "(_F$)|_M$()"))
+
+###############################################################################
+# Make age plots
 filter(d, !(Category %in%c("TOTAL", "WOCBA"))) %>%
-    select(COUNTRY, SUM, Category, Gender) -> d_age
+    select(COUNTRY, year, SUM, Category, Gender) -> d_age
+
 
 d_age$lower <- as.numeric(gsub('A', '', str_extract(d_age$Category, "[0-9]{2}")))
 d_age$upper <- as.numeric(str_extract(d_age$Category, "[0-9]{2}$"))
@@ -51,22 +61,30 @@ label_formatter <- function(x) {
     format(x)
 }
 
-foreach(country=unique(d_age$COUNTRY)) %do% {
-    p <- ggplot(filter(d_age, COUNTRY == country),
+ps <- foreach(country=levels(d_age$COUNTRY)) %do% {
+    p <- ggplot(filter(d_age, COUNTRY == country, year == 2015),
            aes(x=Label, y=SUM, fill=Gender)) +
         geom_bar(subset=.(Gender == "Female"), stat="identity") +
         geom_bar(subset=.(Gender == "Male"), stat="identity") +
         scale_y_continuous(labels = function(x) label_formatter(x)) +
         coord_flip() + 
-        scale_fill_brewer(palette = "Set1") + 
+        scale_fill_brewer(palette = "Paired") + 
         xlab("Age Group") + ylab("Population") +
-        theme_bw() +
-        theme(legend.position=c(.15, .8),
-              legend.title=element_blank())
-    ggsave(paste0('pop_pyramid_', country, '.png'), p, dpi=dpi, width=width, height=height)
-    ggsave(paste0('pop_pyramid_', country, '.eps'), p, dpi=dpi, width=width, height=height)
-
+        theme_bw(base_size=8) +
+        ggtitle(country) +
+        guides(fill=FALSE) +
+    theme(axis.title.x=element_blank(),
+          axis.title.y=element_blank(),
+          axis.ticks.y=element_blank(),
+          axis.text.y=element_blank())
+    # ggsave(paste0('pop_pyramid_', country, '.png'), p, dpi=dpi, width=width, height=height)
+    # ggsave(paste0('pop_pyramid_', country, '.eps'), p, dpi=dpi, width=width, height=height)
+    return(p)
 }
+
+p <- arrangeGrob(ps[[1]], ps[[2]], ps[[3]], ps[[4]], ps[[5]], ps[[6]], ncol=length(ps))
+ggsave(paste0('pop_pyramid_HornCountries.png'), p, dpi=300, width=10, height=2)
+ggsave(paste0('pop_pyramid_HornCountries.eps'), p, dpi=300, width=10, height=2)
 
 # Make overall Horn pyramid
 horn_d <- group_by(d_age, Label, Gender) %>%
@@ -76,10 +94,46 @@ p <- ggplot(horn_d, aes(x=Label, y=SUM, fill=Gender)) +
     geom_bar(subset=.(Gender == "Male"), stat="identity") +
     scale_y_continuous(labels = function(x) label_formatter(x)) +
     coord_flip() + 
-    scale_fill_brewer(palette = "Set1") + 
+    scale_fill_brewer(palette = "Paired") + 
     xlab("Age Group") + ylab("Population") +
     theme_bw() +
     theme(legend.position=c(.15, .8),
           legend.title=element_blank())
 ggsave(paste0('pop_pyramid_HornOfAfrica.png'), p, dpi=dpi, width=width, height=height)
 ggsave(paste0('pop_pyramid_HornOfAfrica.eps'), p, dpi=dpi, width=width, height=height)
+
+###############################################################################
+# Make pop growth plots
+filter(d, Category %in% "TOTAL", Adjusted == TRUE) %>%
+    select(COUNTRY, year, SUM, Category) -> d_total
+
+group_by(d_total, COUNTRY) %>%
+    arrange(year) %>%
+    mutate(pop_start=lag(SUM),
+           pop_end=SUM,
+           growth=c(NA, diff(SUM)),
+           period=paste(lag(year), year, sep="-")) %>%
+    filter(!is.na(growth)) %>%
+    select(-SUM) -> pop_growth
+# Calculate growth rate on an annualized basis
+pop_growth$growth_rate <- ((pop_growth$pop_end / pop_growth$pop_start)^(1/5) - 1) * 100
+
+pop_growth$period <- factor(pop_growth$period)
+
+p <- ggplot(pop_growth, aes(x=as.integer(period), y=growth_rate, 
+                            colour=COUNTRY, linetype=COUNTRY, shape=COUNTRY)) +
+    theme_bw(base_size=10) +
+    geom_line() +
+    geom_point() +
+    scale_x_continuous(breaks=1:length(levels(pop_growth$period)),
+                       labels=levels(pop_growth$period)) +
+    scale_linetype_manual(values=rep(1:3, 2)) +
+    scale_colour_manual(values=rep(brewer.pal(3, "Set2"), 2)) +
+    scale_shape_manual(values=rep(1:3, each=2)) +
+    xlab("Period") + ylab("Annual rate of population growth") +
+    ylim(c(0, 4.5)) +
+    theme(legend.title=element_blank(),
+          legend.key.size=unit(1.5, "cm"),
+          legend.key.height=unit(.5, "cm"))
+ggsave(paste0('pop_growth_HornOfAfrica.png'), p, dpi=dpi, width=width, height=height*1.5)
+ggsave(paste0('pop_growth_HornOfAfrica.eps'), p, dpi=dpi, width=width, height=height*1.5)
