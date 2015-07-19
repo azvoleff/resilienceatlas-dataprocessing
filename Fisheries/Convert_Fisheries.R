05480a4a97b93a3db0ca19b3affd9ed23b9573a9
Alex Zvoleff <azvoleff@gmail.com>
2015-07-19 11:02:37 -0400

    Update for new fisheries data from Cheung et al.

--- Fisheries/Convert_Fisheries.R
+++ Fisheries/Convert_Fisheries.R
@@ -1,14 +1,14 @@
 library(raster)
 
-fish <- stack("H:/Data/GRP/Fisheries_catch/4096.png",
-              bands=as.integer(c(1, 2, 3)))
-proj4string(fish) <- '+init=epsg:4326'
-extent(fish) <- c(-180, 180, -90, 90)
+fish_raw <- read.csv("O:/Data/GRP/Fisheries_catch/CPSRESA1B_converted.csv")
+fish_raw <- as.matrix(fish_raw[, 2:3])
+#fish_raw[fish_raw <= .1] <- NA
 
-writeRaster(fish, "Change_In_Fisheries_Catch_Potential.tif", overwrite=TRUE)
+fish <- brick(nrows=360, ncols=720, nl=2, xmn=-180, xmx=180, 
+              ymn=-90, ymx=90, crs='+init=epsg:4326')
+fish <- setValues(fish, fish_raw)
 
-# 50% increase is equal to RGB: 80, 81, 161
+fish$Year2055DiffPct <- ((fish$Year2055 - fish$Year2000s) / fish$Year2000s) * 100
 
-
-plot(fish)
-plotRGB(fish)
+writeRaster(fish$Year2000s, "O:/Data/GRP/Fisheries_catch/Catch_Potential_2000.tif", overwrite=TRUE)
+writeRaster(fish$Year2055DiffPct, "O:/Data/GRP/Fisheries_catch/Catch_Potential_2055_ChangePercentFrom2000.tif", overwrite=TRUE)
