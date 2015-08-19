c3629fe79ee96b4eea72032e85a4ae60eac1439b
Alex Zvoleff <azvoleff@gmail.com>
2015-08-19 13:04:22 -0400

    Update for full region datasets.

--- Climate_Precipitation_Past/5_trends.R
+++ Climate_Precipitation_Past/5_trends.R
@@ -4,7 +4,8 @@
 
 source('../0_settings.R')
 
-library(rgdal)
+library(tools)
+library(stringr)
 library(raster)
 library(lubridate)
 library(dplyr)
@@ -14,113 +15,85 @@ library(foreach)
 library(doParallel)
 library(spatial.tools)
 
-n_cpus <- 3
+# Select the start and end dates for the data to include in this analysis
+start_date <- as.Date('1985/1/1') # Inclusive
+end_date <- as.Date('2014/12/1') # Exclusive
 
-cl  <- makeCluster(n_cpus)
+cl  <- makeCluster(3)
 registerDoParallel(cl)
 
-# For monthly data:
-dataset <- 'monthly' # For SPI, use monthly
-
-in_folder <- file.path(prefix, "GRP", "CHIRPS")
-out_folder <- file.path(prefix, "GRP", "CHIRPS")
-shp_folder <- file.path(prefix, "GRP", "Boundaries")
+in_folder <- file.path(prefix, "GRP", "CHIRPS-2.0")
+out_folder <- file.path(prefix, "GRP", "CHIRPS-2.0")
 stopifnot(file_test('-d', in_folder))
 stopifnot(file_test('-d', out_folder))
-stopifnot(file_test('-d', shp_folder))
 
-# Note the below code is INCLUSIVE of the start date
-chirps_start_date <- as.Date('1981/1/1')
-# Note the below code is INCLUSIVE of the end date
-chirps_end_date <- as.Date('2014/4/1')
-yrs <- seq(year(chirps_start_date), year(chirps_end_date))
-dates <- seq(chirps_start_date, chirps_end_date, by='months')
-periods_per_year <- 12
+datafiles <- dir(in_folder, pattern='_CHIRPS_monthly_198101-201412.tif$')
+datafile <- datafiles[1]
 
-# Select the start and end dates for the data to include in this analysis
-start_date <- as.Date('1984/1/1') # Inclusive
-end_date <- as.Date('2013/12/1') # Exclusive
-
-aoi_polygons <- readOGR(shp_folder, 'Analysis_Areas')
-aoi_polygons <- aoi_polygons[aoi_polygons$Type == "Landscape", ]
-
-foreach (n=1:nrow(aoi_polygons), .inorder=FALSE,
-         .packages=c("rgdal", "lubridate", "dplyr", "raster",
-                     "rgeos", "teamlucc")) %dopar% {
+foreach (datafile=datafiles) %do% {
     timestamp()
-    aoi <- aoi_polygons[n, ]
-    name <- as.character(aoi$Name)
-    name <- gsub(' ', '', name)
-
+    name <- str_extract(datafile, '^[a-zA-Z]*')
     print(paste0("Processing ", name, "..."))
 
-    in_basename <- file.path(in_folder, paste0(name, '_CHIRPS'))
-
-    out_basename <- file.path(out_folder, paste0(name, '_CHIRPS_',
-                                format(start_date, "%Y%m"), '-', 
-                                format(end_date, "%Y%m")))
-                                  
-    chirps_tif <- paste0(in_basename, "_", dataset, '_', 
-                         format(chirps_start_date, "%Y%m"), '-', 
-                         format(chirps_end_date, "%Y%m"), '.tif')
+    start_date_text <- format(start_date, '%Y%m%d')
+    end_date_text <- format(end_date, '%Y%m%d')
+    out_basename <- file.path(in_folder,
+        paste0(gsub('[0-9-]*', '', file_path_sans_ext(datafile)),
+              start_date_text, '-', end_date_text))
 
     # Calculate the band numbers that are needed
-    included_dates <- dates[(dates >= start_date) & (dates <= end_date)]
+    dates <- seq(as.Date('1981/1/1'), as.Date('2014/12/1'), by='months')
+    dates <- dates[(dates >= start_date) & (dates <= end_date)]
     band_nums <- c(1:length(dates))[(dates >= start_date) & (dates <= end_date)]
 
-    chirps <- stack(chirps_tif, bands=band_nums)
+    ### TEMPORARY
+    band_nums <- 49:84
+    ### TEMPORARY
 
-    # Setup a dataframe with the precipitation data so anomalies, etc. can be 
-    # calculated
-    years <- year(included_dates)
-    years_rep <- rep(years, each=nrow(chirps)*ncol(chirps))
-    subyears <- rep(seq(1, periods_per_year),  length.out=nlayers(chirps))
-    subyears_rep <- rep(subyears, each=nrow(chirps)*ncol(chirps))
-    pixels_rep <- rep(seq(1:(nrow(chirps)*ncol(chirps))), nlayers(chirps))
-    chirps_df <- data.frame(year=years_rep,
-                            subyear=subyears_rep, 
-                            pixel=pixels_rep,
-                            ppt=as.vector(chirps))
-    chirps_df <- tbl_df(chirps_df)
+    chirps <- stack(file.path(in_folder, datafile), bands=band_nums)
 
-    # Map areas that are getting signif. wetter or drier, coded by mm per year
-    extract_coefs <- function(model) {
-        d <- data.frame(summary(model)$coefficients[, c(1, 4)])
-        d <- cbind(row.names(d), d)
-        names(d) <- c('coef', 'estimate', 'p_val')
-        row.names(d) <- NULL
-        return(d)
+    ### TODO: Need to be producing outputs in units of percent change relative 
+    ### to baseline total annual precipitation
+
+    # Function to calculate trend
+    calc_trend <- function(p, dates, ...) {
+        p[p == -9999] <- NA
+        # Setup period identifiers so the data can be used in a dataframe
+        years <- year(dates)
+        years_rep <- rep(years, each=dim(p)[1]*dim(p)[2])
+        subyears <- rep(seq(1, 12),  length.out=dim(p)[3])
+        subyears_rep <- rep(subyears, each=dim(p)[1]*dim(p)[2])
+        pixels_rep <- rep(seq(1:(dim(p)[1]*dim(p)[2])), dim(p)[3])
+        p_df <- data.frame(year=years_rep,
+                           subyear=subyears_rep, 
+                           pixel=pixels_rep,
+                           ppt=as.vector(p))
+        # Map areas that are getting signif. wetter or drier, coded by mm per 
+        # year
+        extract_coefs <- function(model) {
+            d <- data.frame(summary(model)$coefficients[, c(1, 4)])
+            d <- cbind(row.names(d), d)
+            names(d) <- c('coef', 'estimate', 'p_val')
+            row.names(d) <- NULL
+            return(d)
+        }
+        lm_coefs <- group_by(p_df, year, pixel) %>%
+            summarize(ppt_annual=sum(ppt, na.rm=TRUE)) %>%
+            group_by(pixel) %>%
+            do(extract_coefs(lm(ppt_annual ~ year, data=.)))
+        out <- array(c(filter(lm_coefs, coef == "year")$estimate,
+                       filter(lm_coefs, coef == "year")$p_val),
+                     dim=c(dim(p)[1], dim(p)[2], 2))
+        # Mask out nodata areas
+        out[, , 1][is.na(p[ , , 1])] <- NA
+        out[, , 2][is.na(p[ , , 1])] <- NA
+        out
     }
-    annual_ppt_lm_coefs <- group_by(chirps_df, year, pixel) %>%
-        summarize(ppt_annual=sum(ppt, na.rm=TRUE)) %>%
-        group_by(pixel) %>%
-        do(extract_coefs(lm(ppt_annual ~ year, data=.)))
-
-    # Use chirps raster as a template
-    annual_ppt_slope_rast <- brick(chirps, values=FALSE, nl=1)
-    annual_ppt_slope_rast <- setValues(annual_ppt_slope_rast,
-                               matrix(filter(annual_ppt_lm_coefs, coef == "year")$estimate, 
-                                      nrow=nrow(chirps)*ncol(chirps), 
-                                      ncol=1, byrow=TRUE))
-    annual_ppt_slope_rast <- writeRaster(annual_ppt_slope_rast,
-                filename=paste0(out_basename, '_annual_ppt_slope.tif'), 
-                overwrite=TRUE)
-
-    annual_ppt_p_val_rast <- brick(chirps, values=FALSE, nl=1)
-    annual_ppt_p_val_rast <- setValues(annual_ppt_p_val_rast,
-                               matrix(filter(annual_ppt_lm_coefs, coef == "year")$p_val, 
-                                      nrow=nrow(chirps)*ncol(chirps), 
-                                      ncol=1, byrow=TRUE))
-    annual_ppt_p_val_rast <- writeRaster(annual_ppt_p_val_rast,
-                filename=paste0(out_basename, '_annual_ppt_pval.tif'), 
-                overwrite=TRUE)
-
-    annual_ppt_slope_rast <- overlay(annual_ppt_slope_rast, annual_ppt_p_val_rast,
-        fun=function(slp, p) {
-            return(slp * (p < .05))
-        },
-        filename=paste0(out_basename, '_annual_ppt_slope_masked.tif'),
-        overwrite=TRUE)
+    annual_trend <- rasterEngine(p=chirps, args=list(dates=dates),
+        fun=calc_trend, datatype='FLT4S', outbands=2, outfiles=1, 
+        processing_unit="chunk",
+        filename=paste0(out_basename, '_trend_annual'),
+        .packages=c('dplyr', 'lubridate'))
 
 }
 
