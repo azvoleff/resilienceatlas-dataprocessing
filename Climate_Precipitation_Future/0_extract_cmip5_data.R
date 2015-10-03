347a286381f551e9ca6ee99b65bda36aa7011090
Alex Zvoleff <azvoleff@conservation.org>
2015-10-03 15:21:18 -0400

    Finalize for EC2 usage.

--- Climate_Precipitation_Future/0_extract_cmip5_data.R
+++ Climate_Precipitation_Future/0_extract_cmip5_data.R
@@ -11,6 +11,9 @@ library(raster)
 library(foreach)
 library(doParallel)
 
+s3_out_bucket <- 'ci-vsdata'
+s3_out_folder <- '/CHIRPS-2.0/results'
+
 cl <- makeCluster(4)
 registerDoParallel(cl)
 
@@ -36,42 +39,81 @@ files <- arrange(files, scenario, variable, model, period, year)
 files <- filter(files, year >= 1980, year <= 1999,
                 variable == 'pr', scenario == 'historical')
 
-### TESTING ##################
-files <- filter(files, year >= 1980, year <= 1981,
-                variable == 'pr', scenario == 'historical',
-                model %in% c('CNRM-CM5', 'ACCESS1-0'))
-this_model <- files$model[1]
-s3file <- files[1, ]
-### TESTING ##################
+min_year <- 1980
+max_year <- 1981
+start_day <- 1
+stopifnot(start_day >= 1 & start_day <=365)
+# For full year, set end_day to 365. Leap years are handled automatically.
+end_day <- 365
+stopifnot(end_day >= 1 & end_day <=365)
+scenarios <- 'historical'
+variables <- 'pr'
 
 # Loop over models
-mean_ann_tots <- foreach(this_model=unique(files$model),
-                         .packages=c('abind', 'iterators'), 
-                         .combine=abind) %do% {
-    print(this_model)
-    # Loop over files within this model
-    ann_tots <- foreach(s3file=iter(filter(files, model == this_model), by='row'), 
-                        .packages=c('rhdf5'), .combine=abind) %dopar% {
-        print(s3file)
-        outfile <- tempfile(fileext='.hdf')
-        download.file(s3file$url, outfile)
+foreach(this_variable=variables) %:% foreach(this_scenario=scenarios) %do% {
+    file_basename <- paste0(this_variable, '_', this_scenario, '_', min_year, '-', 
+                            max_year, '_', start_day, '-', end_day)
+    these_files <- filter(files, year >= min_year, year <= max_year,
+                          variable == this_variable, scenario == this_scenario,
+                          model %in% c('CNRM-CM5', 'ACCESS1-0'))
 
-        d <- h5read(outfile, paste0('/', s3file$variable))
-        d <- apply(d, c(1, 2), sum)
+    mean_totals <- foreach(this_model=unique(these_files$model),
+                           .packages=c('abind', 'iterators'),
+                           .combine=abind) %do% {
+        print(paste0('Processing ', this_model))
 
-        # Calculate annual total and return as a 1 layer array
-        return(array(d, dim=c(dim(d), 1)))
+        # Loop over files within this model
+        totals <- foreach(s3file=iter(filter(these_files, model == this_model), by='row'), 
+                            .packages=c('rhdf5'), .combine=abind) %dopar% {
+            print(s3file)
+            temp_file <- tempfile(fileext='.hdf')
+            download.file(s3file$url, temp_file)
+
+            d <- h5read(temp_file, paste0('/', s3file$variable))
+            # TODO: Handle case of end day that is less than start day, meaning 
+            # that the season spans two calendar years.
+            if (!(start_day == 1 & end_day == 365)) {
+                if (end_day == 365) {
+                    if (dim(d)[3] == 366) {
+                        this_end_day <- 366
+                    } else {
+                        this_end_day <- 365
+                    }
+                } else {
+                    this_end_day <- end_day
+                }
+                d <- d[, , start_day:this_end_day]
+            }
+            
+            d <- apply(d, c(1, 2), sum)
+
+            # Calculate annual total and return as a 1 layer array
+            return(array(d, dim=c(dim(d), 1)))
+        }
+
+        # Write array to S3
+        out <- brick(nrows=720, ncols=1440, nl=dim(totals)[3],
+                     xmn=-180, xmx=180, ymn=-90, ymx=90, crs='+init=epsg:4326')
+        out <- setValues(out, totals)
+
+        out_file <-  file.path(out_folder, paste0(file_basename, "_", this_model, ".tif"))
+        writeRaster(out, out_file, overwrite=TRUE)
+        system(paste('s3put -b', s3_out_bucket, '-p', out_folder, '-k', s3_out_folder, out_file))
+        
+        # Calculate mean for this model, and make it into a 1 layer array
+        mod_mean <- apply(totals, c(1, 2), mean)
+
+        # Transpose so raster will appear correctly.
+        mod_mean <- t(mod_mean)
+
+        return(array(mod_mean, dim=c(dim(mod_mean), 1)))
     }
-    # Calculate mean for this model, and make it into a 1 layer array
-    mod_mean <- apply(ann_tots, c(1, 2), mean)
 
-    # Write model mean to disk
-    
-    return(array(mod_mean, dim=c(dim(mod_mean), 1)))
+    out <- brick(nrows=720, ncols=1440, nl=dim(mean_totals)[3],
+                 xmn=-180, xmx=180, ymn=-90, ymx=90, crs='+init=epsg:4326')
+    out <- setValues(out, mean_totals)
+    out_file <- file.path(out_folder, paste0(file_basename, '_modelmeans.tif'))
+    writeRaster(out, out_file, overwrite=TRUE)
+
+    system(paste('s3put -b', s3_out_bucket, '-p', out_folder, '-k', s3_out_folder, out_file))
 }
-
-out <- brick(nrows=720, ncols=1440, nl=dim(mean_ann_tots)[3],
-             xmn=-180, xmx=180, ymn=-90, ymx=90, crs='+init=epsg:4326')
-out <- setValues(out, mean_ann_tots)
-writeRaster(out, file.path(out_folder, "mean_ann_tots.tif"), overwrite=TRUE)
-
