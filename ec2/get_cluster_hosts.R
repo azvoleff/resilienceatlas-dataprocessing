d4e1a3497b750704da590a48c78bbace4dc4a553
Alex Zvoleff <azvoleff@conservation.org>
2015-10-08 12:04:09 -0400

    Fix hostname extraction.

--- ec2/get_cluster_hosts.R
+++ ec2/get_cluster_hosts.R
@@ -2,10 +2,10 @@ library(stringr)
 
 get_cluster_hosts <- function() {
     con  <- file('/etc/hosts', open="r")
-    cluster_hosts <- list()
+    cluster_hosts <- c()
     while (length(this_line <- readLines(con, n = 1, warn = FALSE)) > 0) {
         if(grepl('(node[0-9]{3})|(master)', this_line)) {
-            cluster_hosts <- c(cluster_hosts, str_extract('(node[0-9]{3})|(master)', this_line))
+            cluster_hosts <- c(cluster_hosts, str_extract(this_line, '(node[0-9]{3})|(master)'))
         }
     } 
     close(con)
