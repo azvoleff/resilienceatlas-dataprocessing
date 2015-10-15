6c1d09c8a86865ed144271e1e97d789687707708
Alex Zvoleff <azvoleff@conservation.org>
2015-10-15 19:09:42 -0400

    Bugfix on ec2.

--- Climate_Future/2_make_change_layers.R
+++ Climate_Future/2_make_change_layers.R
@@ -3,19 +3,22 @@ library(dplyr)
 library(foreach)
 library(stringr)
 library(abind)
+library(doParallel)
 
 source('../0_settings.R')
 
 source('../ec2/s3_ls.R')
 
+source('../ec2/get_cluster_hosts.R')
+cl <- makeCluster(rep(get_cluster_hosts(), each=4))
+registerDoParallel(cl)
+
 in_folder <- file.path(prefix, "GRP", "CMIP5")
 
+s3_in <- 's3://ci-vsdata/CMIP5/seasonal_totals/'
 s3_out <- 's3://ci-vsdata/CMIP5/results/'
 
-s3_files <- s3_ls(s3_out)
-
-# Ignore the modelmeans files
-s3_files <- s3_files[!grepl('modelmeans', s3_files$file), ]
+s3_files <- s3_ls(s3_in)
 
 s3_files$scenario <- str_extract(s3_files$file, '(historical)|(rcp(45|85))')
 s3_files$variable <- str_extract(s3_files$file, '^[a-z]*')
@@ -23,12 +26,12 @@ s3_files$period <- str_extract(s3_files$file, '[0-9]{4}-[0-9]{4}')
 s3_files$season <- gsub('_', '', str_extract(s3_files$file, '_[0-9]{1,3}-[0-9]{1,3}_'))
 
 ## testing
-this_variable <- 'pr'
-this_season <- '1-365'
+# this_variable <- 'pr'
+# this_season <- '1-365'
 ## testing
 
 foreach(this_variable=unique(s3_files$variable)) %:%
-    foreach(this_season=unique(s3_files$variable)) %do% {
+    foreach(this_season=unique(s3_files$season)) %do% {
 
     base_files <- filter(s3_files, variable == this_variable,
                          season == this_season, scenario == 'historical')
@@ -38,7 +41,7 @@ foreach(this_variable=unique(s3_files$variable)) %:%
     # Calculate baseline mean (base_m)
     base_m <- foreach(base_file=base_files$file, .combine=stack) %dopar% {
         temp_file <- tempfile(fileext='.tif')
-        system2('aws', args=c('s3', 'cp', paste0(s3_out, base_file), temp_file))
+        system2('aws', args=c('s3', 'cp', paste0(s3_in, base_file), temp_file))
         model_data <- brick(temp_file)
         mod_mean <- mean(model_data)
         unlink(temp_file)
@@ -69,7 +72,7 @@ foreach(this_variable=unique(s3_files$variable)) %:%
                                  period == this_period)
                 scen_m <- foreach(scen_file=scen_files$file, .combine=stack) %do% {
                     temp_file <- tempfile(fileext='.tif')
-                    system2('aws', args=c('s3', 'cp', paste0(s3_out, scen_file), temp_file))
+                    system2('aws', args=c('s3', 'cp', paste0(s3_in, scen_file), temp_file))
                     model_data <- brick(temp_file)
                     mod_mean <- mean(model_data)
                     unlink(temp_file)
