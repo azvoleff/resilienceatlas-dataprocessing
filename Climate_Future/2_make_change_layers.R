72561df084df2d4e45a4de8747ea6ac090f2ddf0
Alex Zvoleff <azvoleff@conservation.org>
2015-10-18 17:37:46 -0400

    Clean file handling.

--- Climate_Future/2_make_change_layers.R
+++ Climate_Future/2_make_change_layers.R
@@ -1,9 +1,12 @@
+library(rgdal)
 library(raster)
 library(dplyr)
 library(foreach)
 library(stringr)
-library(abind)
 library(doParallel)
+
+print('Setting up...')
+timestamp()
 
 source('../0_settings.R')
 
@@ -61,22 +64,24 @@ s3_files <- filter(s3_files, season == 'annual')
 print('Processing...')
 timestamp()
 foreach(this_variable=unique(s3_files$variable)) %:%
-    foreach(this_season=unique(s3_files$season)) %do% {
+    foreach(this_season=unique(s3_files$season),
+            .packages=c('raster', 'dplyr', 'foreach', 'rgdal')) %dopar% {
 
-    base_files <- filter(s3_files, variable == this_variable,
-                         season == this_season, scenario == 'historical')
+    these_files <- filter(s3_files, variable == this_variable,
+                          season == this_season)
+
+    base_files <- filter(these_files, scenario == 'historical')
     stopifnot(length(unique(base_files$agg_period)) == 1)
     base_agg_period <- base_files$agg_period[1]
 
     # Calculate baseline mean (base_m)
-    base_m <- foreach(this_model=unique(base_files$model), .combine=stack,
-                      .packages=c('raster', 'dplyr')) %dopar% {
+    base_m <- foreach(this_model=unique(base_files$model), .combine=stack) %do% {
         temp_dir <- get_tempdir()
-        these_files <- filter(base_files, model == this_model)$file
-        foreach(this_file=these_files) %do% {
+        these_base_files <- filter(base_files, model == this_model)$file
+        foreach(this_file=these_base_files) %do% {
             system2('aws', args=c('s3', 'cp', paste0(s3_in, this_file), temp_dir))
         }
-        model_data <- stack(file.path(temp_dir, these_files))
+        model_data <- stack(file.path(temp_dir, these_base_files))
         mod_mean <- mean(model_data)
         unlink(temp_dir, recursive=TRUE)
         return(mod_mean)
@@ -100,26 +105,23 @@ foreach(this_variable=unique(s3_files$variable)) %:%
     system2('aws', args=c('s3', 'cp', temp_file, s3_out_base_mmsd))
 
     # Calculate scenario model means
-    scenarios <- unique(s3_files$scenario[s3_files$scenario != 'historical'])
-    agg_periods <- unique(s3_files$agg_period[s3_files$scenario != 'historical'])
+    scenarios <- unique(these_files$scenario[these_files$scenario != 'historical'])
+    agg_periods <- unique(these_files$agg_period[these_files$scenario != 'historical'])
     foreach(this_scenario=scenarios) %:% 
         foreach(this_agg_period=agg_periods,
                 .packages=c('raster', 'dplyr', 'foreach')) %do% {
 
-        scen_files <- filter(s3_files,
-                             variable == this_variable,
-                             season == this_season,
+        scen_files <- filter(these_files,
                              scenario == this_scenario,
                              agg_period == this_agg_period)
 
-        scen_m <- foreach(this_model=unique(scen_files$model), .combine=stack,
-                          .packages=c('raster', 'dplyr')) %dopar% {
+        scen_m <- foreach(this_model=unique(scen_files$model), .combine=stack) %do% {
             temp_dir <- get_tempdir()
-            these_files <- filter(scen_files, model == this_model)$file
-            foreach(this_file=these_files) %do% {
+            these_scen_files <- filter(scen_files, model == this_model)$file
+            foreach(this_file=these_scen_files) %do% {
                 system2('aws', args=c('s3', 'cp', paste0(s3_in, this_file), temp_dir))
             }
-            model_data <- stack(file.path(temp_dir, these_files))
+            model_data <- stack(file.path(temp_dir, these_scen_files))
             mod_mean <- mean(model_data)
             unlink(temp_dir, recursive=TRUE)
             return(mod_mean)
