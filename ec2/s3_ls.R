261bc3b12740db12247b72408f7b4d71fffaf412
Alex Zvoleff <azvoleff@conservation.org>
2015-10-18 17:36:51 -0400

    Vectorize for efficiency.

--- ec2/s3_ls.R
+++ ec2/s3_ls.R
@@ -1,5 +1,6 @@
 # Function to parse output of S3 ls command
-# Note env is needed on windows to 
+#
+# config is needed on windows to allow aws CLI to find access key
 s3_ls <- function(s3_folder, config=NULL) {
     require(stringr)
     if (!is.null(config)) {
@@ -7,15 +8,12 @@ s3_ls <- function(s3_folder, config=NULL) {
         Sys.setenv(AWS_CONFIG_FILE=config)
     }
     s3_ls <- system2('aws', args=c('s3', 'ls', s3_folder), stdout=TRUE)
-    parse_out <- function(x) {
-        # Replace any combination of repeated spaces with a single space
-        x <- gsub(' [ ]*', ' ', x)
-        x <- unlist(str_split(x, ' '))
-        data.frame(date=x[1], time=x[2], size=x[3], file=x[4])
-    }
-    s3_files <- plyr::ldply(s3_ls, parse_out)
-    # Filter out empty placeholder file with size 1 and no filename:
-    s3_files <- s3_files[s3_files$file != '', ]
+    s3_files <- gsub(' [ ]*', ' ', s3_ls)
+    s3_files <- str_split(s3_files, ' ')
+    # Drop empty placeholder file with size 1 and no filename:
+    s3_files <- s3_files[-1]
+    s3_files <- data.frame(matrix(unlist(s3_files), nrow=length(s3_files), byrow=T))
+    names(s3_files) <- c("date", "time", "size", "file")
     s3_files$file <- as.character(s3_files$file)
     return(s3_files)
 }
