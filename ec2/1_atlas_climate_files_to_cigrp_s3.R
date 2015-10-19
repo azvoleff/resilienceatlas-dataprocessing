1d7b1c5aa2c9a57fe09932d490019e2326b6b2a5
Alex Zvoleff <azvoleff@gmail.com>
2015-10-19 18:07:24 -0400

    Update to fix NA values where possible.

--- ec2/1_atlas_climate_files_to_cigrp_s3.R
+++ ec2/1_atlas_climate_files_to_cigrp_s3.R
@@ -21,17 +21,22 @@ chirps_folder <- file.path(prefix, 'GRP', 'CHIRPS-2.0')
 chirps_monthlytotal_in <- paste0(regions, '_CHIRPS_monthly_198101-201412_mean_monthly.gri')
 chirps_monthlytotal_in <- file.path(chirps_folder, chirps_monthlytotal_in)
 chirps_monthlytotal_out <- paste0(s3_folder, '/Rainfall/Historical/CHIRPS_19850101-20141201_monthlyppt_total_mm.tif')
-chirps_monthlytotal_mosaic <- mosaic_rasters(chirps_monthlytotal_in,
-                                            chirps_monthlytotal_out,
-                                            output_Raster=TRUE)
+vrtfile <- tempfile(fileext='.vrt')
+gdalbuildvrt(chirps_monthlytotal_in, vrtfile, vrtnodata=-9999)
+chirps_monthlytotal_mosaic <- mosaic_rasters(vrtfile,
+                                             chirps_monthlytotal_out,
+                                             output_Raster=TRUE)
 
 chirps_annualtotal_mosaic <- calc(chirps_monthlytotal_mosaic, fun=sum, filename=paste0(s3_folder, '/Rainfall/Historical/CHIRPS_19850101-20141201_annualppt_total_mm.tif'), overwrite=TRUE)
 
 # Trend
+# TODO: NAs are not showing properly for this layer
 chirps_trend_in <- paste0(regions, '_CHIRPS_monthly_19850101-20141201_trend_annual.gri')
 chirps_trend_in <- file.path(chirps_folder, chirps_trend_in)
 chirps_trend_out <- paste0(s3_folder, '/Rainfall/Historical/CHIRPS_19850101-20141201_annualppt_trend_pct-per-decade.tif')
-chirps_trend_mosaic <- mosaic_rasters(chirps_trend_in,
+vrtfile <- tempfile(fileext='.vrt')
+gdalbuildvrt(chirps_trend_in, vrtfile, vrtnodata=-9999)
+chirps_trend_mosaic <- mosaic_rasters(vrtfile,
                                       chirps_trend_out,
                                       output_Raster=TRUE)
 
@@ -40,8 +45,17 @@ chirps_trend_mosaic <- mosaic_rasters(chirps_trend_in,
 cru_folder <- file.path(prefix, 'GRP', 'CRU')
 
 # Climatology
+cru_monthlymean_in <- paste0(regions, '_cru_ts3.23_tmp_1901.2014_mean_monthly.gri')
+cru_monthlymean_in <- file.path(cru_folder, cru_monthlymean_in)
+cru_monthlymean_out <- paste0(s3_folder, '/Temperature/Historical/cru_ts3.23_19850101-20141201_tmp_monthlymean_deg.tif')
+vrtfile <- tempfile(fileext='.vrt')
+gdalbuildvrt(cru_monthlymean_in, vrtfile, vrtnodata=-9999)
+cru_monthlymean_mosaic <- mosaic_rasters(vrtfile,
+                                         cru_monthlymean_out,
+                                         output_Raster=TRUE)
 
-#TODO: Need to calculate CRU climatology
+cru_annualmean_mosaic <- calc(cru_monthlymean_mosaic, fun=mean, filename=paste0(s3_folder, 'cru_ts3.23_19850101-20141201_tmp_annualmean_deg.tif'), overwrite=TRUE)
+
 
 # Trend
 cru_tmp_trend_in <- paste0(regions, '_cru_ts3.23_tmp_decadal_slope.tif')
@@ -54,7 +68,6 @@ cru_tmp_trend_mosaic <- mosaic_rasters(cru_tmp_trend_in,
 ###############################################################################
 # Climate projections
 cmip5_folder <- file.path(prefix, 'GRP', 'CMIP5')
-
 
 # Precipitation projections
 cmip5_pr_files <- list.files(cmip5_folder, pattern='^pr_')
