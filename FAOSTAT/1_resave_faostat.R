1f273acc422bb88c6ec52ca01209fe19ade24e11
Alex Zvoleff <azvoleff@gmail.com>
2016-08-19 10:55:52 -0400

    Fix for all countries.

--- FAOSTAT/1_resave_faostat.R
+++ FAOSTAT/1_resave_faostat.R
@@ -3,9 +3,6 @@ source('../0_settings.R')
 library(tools)
 library(dplyr)
 
-countries <- read.csv(file.path(prefix, "GRP", "DataTables", 
-                                "GRP_Countries.csv"), stringsAsFactors=FALSE)
-
 # Read the FAOSTAT numeric country code to ISO3 mapping table
 fao_iso_key <- read.csv(file.path(prefix, "GRP", "FAOSTAT", "FAOSTAT_ISO_CODES.csv"))
 
@@ -18,11 +15,10 @@ reformat_fao_data <- function(faofile, d) {
     # Filter out CountryCode values above 5000 as these are aggregated metrics
     d <- filter(d, CountryCode < 5000)
     d <- left_join(d, select(fao_iso_key, FAOSTAT, ISO3), by=c("CountryCode"="FAOSTAT"))
-    d <- filter(d, ISO3 %in% countries$ISO3)
     save(d, file=file.path(prefix, "GRP", "FAOSTAT", 
-                           paste0(file_path_sans_ext(faofile), "_GRP.RData")))
+                           paste0(file_path_sans_ext(faofile), "_iso.RData")))
     write.csv(d, file=file.path(prefix, "GRP", "FAOSTAT", 
-                           paste0(file_path_sans_ext(faofile), "_GRP.csv")), 
+                           paste0(file_path_sans_ext(faofile), "_iso.csv")), 
               row.names=FALSE)
 }
 
