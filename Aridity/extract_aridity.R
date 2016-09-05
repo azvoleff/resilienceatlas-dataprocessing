9244f141be083d409fbf699f9687bb00454c887f
Alex Zvoleff <azvoleff@gmail.com>
2016-09-05 11:15:21 +0300

    Rename for clarity.

--- Aridity/extract_aridity.R
+++ Aridity/extract_aridity.R
@@ -8,6 +8,19 @@ a <- aggregate(a, fact=10, fun=mean)
 
 writeRaster(a, 'global_aridity_10km.tif', overwrite=TRUE)
 
+drylands <- calc(a, function(x) {
+        x[x < 300 | x > 6500] <- NA
+        x[x >= 300 & x <= 6500] <- 1
+        x
+    }, filename='global_aridity_10km_drylands.tif', overwrite=TRUE)
+
+# Use gdal_polygonize to convert drylands to a polygon (needed to run on Linux 
+# machine). Command: gdal_polygonize.py global_aridity_10km_drylands.tif -f 
+# "GeoJSON" global_aridity_10km_drylands_poly.json"
+
+#a_polys <- rasterToPolygons(drylands, fun=function(x) {x > 300 & x < 6500}, 
+#dissolve=TRUE)
+
 # Topcode aridity at 2.5, then convert to byte image
 a <- calc(a, function(x) {
         x <- x * .0001
